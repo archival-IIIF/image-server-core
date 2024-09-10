f71126b2a6eab05fb9a17d3ff0e4a668f22a27f1
kerim1 <kerim.meijer@di.huc.knaw.nl>
2024-09-10 11:49:08 +0200

    Update dependencies and web structure

--- src/web.ts
+++ src/web.ts
@@ -1,8 +1,8 @@
 import sharp from 'sharp';
 import {join} from 'path';
-import {parse} from 'url';
 import {parseArgs} from 'util';
-import {createServer} from 'http';
+import {parse, UrlWithParsedQuery} from 'url';
+import {createServer, ServerResponse} from 'http';
 
 import serveImage from './imageServer';
 import {NotImplementedError, RequestError} from './errors';
@@ -42,50 +42,58 @@ if (debug)
 console.log('Dependencies:');
 console.table(sharp.versions);
 
-console.log(`Available image input formats: ${(Object.values(sharp.format) as sharp.AvailableFormatInfo[])
+console.log(`Available image input formats: ${(Object.values(sharp.format))
     .filter(format => format.input.file)
     .map(format => format.id)
     .join(', ')}`);
 
-console.log(`Available image output formats: ${(Object.values(sharp.format) as sharp.AvailableFormatInfo[])
+console.log(`Available image output formats: ${(Object.values(sharp.format))
     .filter(format => format.output.buffer)
     .map(format => format.id)
     .join(', ')}`);
 
 console.log(`Number of image processing threads: ${sharp.concurrency()}`);
 
+async function requestImage(parsedUrl: UrlWithParsedQuery, parts: string[], res: ServerResponse) {
+    const imagePath = decodeURIComponent(parts[0]);
+    const region = parts[1];
+    const size = parts[2];
+    const rotation = parts[3];
+    const quality = parts[4].substring(0, parts[4].indexOf('.'));
+    const format = parts[4].substring(parts[4].indexOf('.') + 1);
+
+    debug && console.log(`Received a request for an image on path ${imagePath}`);
+
+    const path = join(root as string, imagePath);
+    const maxSize = Array.isArray(parsedUrl.query.max)
+        ? parseInt(parsedUrl.query.max[0])
+        : parsedUrl.query.max
+            ? parseInt(parsedUrl.query.max)
+            : null;
+
+    const image = await serveImage(path, maxSize, {region, size, rotation, quality, format});
+
+    if (image.contentType) res.setHeader('Content-Type', image.contentType);
+    if (image.contentLength) res.setHeader('Content-Length', String(image.contentLength));
+    res.setHeader('Content-Disposition', `inline; filename="${imagePath}-${region}-${size}-${rotation}-${quality}.${format}"`);
+    res.write(image.image);
+    res.end();
+
+    debug && console.log(`Sending an image on path ${imagePath}`);
+}
+
 const server = createServer(async (req, res) => {
     try {
-        const parsedUrl = parse(req.url as string, true);
-        const path = parsedUrl.pathname as string;
+        const parsedUrl = parse(req.url!, true);
+        const path = parsedUrl.pathname!;
         const parts = path.substring(1).split('/');
 
         if (parts.length === 5 && parts[4].indexOf('.') >= 0) {
-            const imagePath = decodeURIComponent(parts[0]);
-            const region = parts[1];
-            const size = parts[2];
-            const rotation = parts[3];
-            const quality = parts[4].substring(0, parts[4].indexOf('.'));
-            const format = parts[4].substring(parts[4].indexOf('.') + 1);
-
-            debug && console.log(`Received a request for an image on path ${imagePath}`);
-
-            const path = join(root as string, imagePath);
-            const maxSize = Array.isArray(parsedUrl.query.max)
-                ? parseInt(parsedUrl.query.max[0])
-                : parsedUrl.query.max
-                    ? parseInt(parsedUrl.query.max)
-                    : null;
-
-            const image = await serveImage(path, maxSize, {region, size, rotation, quality, format});
-
-            if (image.contentType) res.setHeader('Content-Type', image.contentType);
-            if (image.contentLength) res.setHeader('Content-Length', String(image.contentLength));
-            res.setHeader('Content-Disposition', `inline; filename="${imagePath}-${region}-${size}-${rotation}-${quality}.${format}"`);
-            res.write(image.image);
+            await requestImage(parsedUrl, parts, res);
+        }
+        else if (parts.length === 1) {
+            res.writeHead(302, {'Location': parsedUrl.pathname! + '/info.json'});
             res.end();
-
-            debug && console.log(`Sending an image on path ${imagePath}`);
         }
         else {
             res.writeHead(404);
